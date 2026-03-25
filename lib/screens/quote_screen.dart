302b1bf0f1fe8f194e0dbfad9460cf8c0c326da3
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-03-25 16:36:49 +0530

    adding login and signup

--- lib/screens/quote_screen.dart
+++ lib/screens/quote_screen.dart
@@ -1,58 +1,86 @@
 import 'package:flutter/material.dart';
 
-class QuoteScreen extends StatelessWidget{
+class QuoteScreen extends StatelessWidget {
   const QuoteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-    appBar: AppBar(title: Text("Quote Screen"),
-    ),
-    body: SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 50,
-              width: 300,
-              color: Colors.black,
-              child: Center(child: Text("Login",style: TextStyle(),)),
-            ),
-            SizedBox(height: 20,),
-            Container(
-              height: 200,
-              width: 200,
-              color: Colors.black,
-              child: Center(child: Text("this is cloumn container.")),
-            ),
-             SizedBox(height: 20,),
-            Container(
-              height: 200,
-              width: 200,
-              color: Colors.black,
-              child: Center(child: Text("this is cloumn container.")),
-            ),
-             SizedBox(height: 20,),
-            Container(
-              height: 200,
-              width: 200,
-              color: Colors.black,
-              child: Center(child: Text("this is cloumn container.")),
-            ),
-             SizedBox(height: 20,),
-            Container(
-              height: 200,
-              width: 200,
-              color: Colors.black,
-              child: Center(child: Text("this is cloumn container.")),
-            ),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("Quote Screen", style: TextStyle(color: Colors.white)),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.white,
+                child: Center(child: Text("this is cloumn container.")),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.white,
+                child: Center(child: Text("this is cloumn container.")),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                height: 50,
+                width: 300,
+                color: Colors.white,
+                child: Center(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 33,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 50,
+                width: 300,
+                color: Colors.white,
+                child: Center(
+                  child: Text(
+                    "SignUp",
+                    style: TextStyle(
+                      fontSize: 33,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ),
+              ),
 
-          ],
+              SizedBox(height: 20),
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.black,
+                child: Center(child: Text("this is cloumn container.")),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.black,
+                child: Center(child: Text("this is cloumn container.")),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-   );
+    );
   }
 }
