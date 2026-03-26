c26207239d55f30b4623430ff2e989e50c04fdf9
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-03-26 12:29:31 +0530

    added text and buttons

--- lib/screens/quote_screen.dart
+++ lib/screens/quote_screen.dart
@@ -1,18 +1,65 @@
 import 'package:flutter/material.dart';
 
-class QuoteScreen extends StatefulWidget{
+class QuoteScreen extends StatefulWidget {
   const QuoteScreen({super.key});
 
   @override
-  State<QuoteScreen> createState ()=> _QuoteScreenState();
+  State<QuoteScreen> createState() => _QuoteScreenState();
 }
 
-class _QuoteScreenState extends State<QuoteScreen>{
-  
+class _QuoteScreenState extends State<QuoteScreen> {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-    appBar: AppBar(),
-   );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        toolbarHeight: 80,
+        title: Center(
+          child: Text(
+            "Quote Generator App",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 27,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+        ),
+      ),
+      backgroundColor: const Color.fromARGB(81, 53, 16, 16),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            SizedBox(height: 40),
+
+            Text(
+              "-Albert Einstein",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                fontWeight: FontWeight.w100,
+              ),
+            ),
+
+            SizedBox(height: 40,),
+
+            ElevatedButton(
+              onPressed: () {
+                print("Button Pressed");
+              },
+              child: Text("inspire me"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
