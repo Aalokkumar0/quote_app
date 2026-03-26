a3b23d65232149bac73b741843c1e498e3a12593
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-03-26 12:03:19 +0530

    statefull const

--- lib/screens/quote_screen.dart
+++ lib/screens/quote_screen.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-class QuoteScreen extends StatefulWidget {
+class QuoteScreen extends StatefulWidget{
   const QuoteScreen({super.key});
 
   @override
-  State<QuoteScreen>createState ()=> _QuotescreenState ();
+  State<QuoteScreen> createState ()=> _QuoteScreenState();
 }
 
-class _QuotescreenState extends State <QuoteScreen>{
-
+class _QuoteScreenState extends State<QuoteScreen>{
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-    );
+   return Scaffold(
+    appBar: AppBar(),
+   );
   }
 }
